0ab1fae0d5ba497c4361dce057a972a5c9a6e962
Simme63 <simon.wandel@hotmail.com>
2025-01-13 11:30:26 +0100

    config stuff pls work pls

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      "Access-Control-Allow-Origin": "https://server-wheat-alpha.vercel.app/",
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Credentials': 'true'
+    },
+  }
 })
